c6f80f519208dcf1ecaaff831aeed95f5d022d56
Michaelangelo <jagbodyke01@gmail.com>
2025-11-20 20:45:09 +0000

    Deployment commit

---
+++ da34866c6baa37199fb3.js
@@ -0,0 +1,6 @@
+import loadHomePage from './home-page-builder.js';
+import loadMenuPage from './menu-page-builder.js';
+import loadContactPage from './contact-page-builder.js';
+
+import "./styles.css";
+//Variables
